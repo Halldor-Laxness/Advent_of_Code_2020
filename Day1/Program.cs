d31a821b0122ca67034c8caa92b3c7e1567803f4
Danyang <wdanyang1218@gmail.com>
2021-04-05 01:10:00 +0200

    Outcome is  440979

---
+++ Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("/Users/danyang/Code/Advent of Code/Day1/input.txt");
+            List<int> data = (Array.ConvertAll(input, s => Int32.Parse(s))).ToList();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                var target = 2020 - data[i];
+
+                if(data.Contains(target))
+                {
+                    Console.WriteLine("OutCome: {0}", target * data[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
